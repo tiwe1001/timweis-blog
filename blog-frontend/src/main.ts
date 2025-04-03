4a664ed93feff0a22e8cd255734ba911d7e018e0
Tim Weis <tiwe1001@stud.hs-kl.de>
2025-04-03 19:55:35 +0200

    added frontend with vue3-framework

---
+++ blog-frontend/src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import { createVuetify } from 'vuetify';
+
+const vuetify = createVuetify();
+
+createApp(App).use(store).use(router).use(vuetify).mount('#app')
